a40e7cf69629d1b82e5fc29717ed28b538366a1a
Augusto Acual <augustoacual@gmail.com>
2024-02-10 10:49:58 -0600

    Fix issue modal CSS move into the body tag

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, ElementRef } from '@angular/core'
 import { ModalService } from '../../services/modal.service'
 
 @Component({
@@ -9,7 +9,11 @@ import { ModalService } from '../../services/modal.service'
 export class ModalComponent {
   @Input() modalID = ''
 
-  constructor(public modal: ModalService) {
+  constructor(public modal: ModalService, public el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
