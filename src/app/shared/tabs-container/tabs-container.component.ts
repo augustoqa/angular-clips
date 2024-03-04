8ad91a959b64e100cb677f79181bd193fd2b9ada
Augusto Acual <augustoacual@gmail.com>
2024-03-03 18:21:53 -0600

    Using the ContentChildren decorator for access the children of TabsContainerComponent

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren } from '@angular/core'
+import { TabComponent } from '../tab/tab.component'
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent {
+  @ContentChildren(TabComponent) tabs = {}
 
+  ngAfterContentInit() {
+    console.log(this.tabs)
+  }
 }
