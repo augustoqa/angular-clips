9041a941cdfc8a206a6f39764a4fdd0bd9804e79
Augusto Acual <augustoacual@gmail.com>
2024-03-05 19:04:34 -0600

    Setting an Active Tab

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -16,6 +16,16 @@ export class TabsContainerComponent implements AfterContentInit {
     new QueryList()
 
   ngAfterContentInit() {
-    console.log(this.tabs)
+    const activeTabs = this.tabs?.filter((tab) => tab.active)
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((tab) => (tab.active = false))
+
+    tab.active = true
   }
 }
