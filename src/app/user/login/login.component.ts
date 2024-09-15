73da97dc20ab64454e2242dcfbf8ddb629cdc392
Augusto Quisquinay <augustoacual@gmail.com>
2024-09-15 11:34:51 -0600

    Two-way Binding - Template Forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  credentials = {
+    email: '',
+    password: '',
+  }
 }
