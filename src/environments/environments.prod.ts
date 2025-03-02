59f019754eeb23a655782aa8a3b437b335b140f6
Augusto Quisquinay <augustoacual@gmail.com>
2025-03-02 10:34:14 -0600

    Installing AngularFire and Import

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDAwJK9mvKrrb9j8lyWc8BoHIXw0SVJ5G4',
+    authDomain: 'clips-2545e.firebaseapp.com',
+    projectId: 'clips-2545e',
+    storageBucket: 'clips-2545e.firebasestorage.app',
+    appId: '1:1071575147639:web:eb76dff9b320a58bcdd272',
+  },
+}
