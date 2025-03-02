1344b18fddac58573fbc89fed3a209626537ef25
Augusto Quisquinay <augustoacual@gmail.com>
2025-03-02 11:14:18 -0600

    Authentication - User Registration

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-register',
@@ -7,14 +8,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  name = new FormControl('', [
-    Validators.required,
-    Validators.minLength(3),
-  ])
-  email = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ])
+  constructor(private auth: AngularFireAuth) {}
+
+  name = new FormControl('', [Validators.required, Validators.minLength(3)])
+  email = new FormControl('', [Validators.required, Validators.email])
   age = new FormControl('', [
     Validators.required,
     Validators.min(18),
@@ -24,9 +21,7 @@ export class RegisterComponent {
     Validators.required,
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ])
-  confirm_password = new FormControl('', [
-    Validators.required,
-  ])
+  confirm_password = new FormControl('', [Validators.required])
   phoneNumber = new FormControl('', [
     Validators.required,
     Validators.minLength(13),
@@ -46,9 +41,16 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your account is being created.'
   alertColor = 'blue'
 
-  register() {
+  async register() {
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created.'
     this.alertColor = 'blue'
+
+    const { email, password } = this.registerForm.value
+
+    const userCred = await this.auth.createUserWithEmailAndPassword(
+      email as string,
+      password as string
+    )
   }
 }
