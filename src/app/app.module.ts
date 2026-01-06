e6cd5a56bf3ed20bec1ab681e32411340254abc0
Augustoa Acual <augustoacual@gmail.com>
2026-01-06 10:15:11 -0600

    Routing - Wildcard Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,27 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth'
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
-import { VideoModule } from './video/video.module';
+import { VideoModule } from './video/video.module'
 import { ClipComponent } from './clip/clip.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent, ClipComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ClipComponent,
+    NotFoundComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
     VideoModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
